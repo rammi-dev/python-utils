c70040dae604ddd77750fbf296ac04b31d35d14d
rammi-dev <krystalografia@gmail.com>
2026-01-15 20:48:30 +0100

    removed old repo

--- dlh-dbt-airflow-lib/src/dlh_airflow_common/operators/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Common Airflow operators."""
-
-from dlh_airflow_common.operators.base import BaseOperator
-from dlh_airflow_common.operators.dbt import DbtOperator
-
-__all__ = ["BaseOperator", "DbtOperator"]
--- dlh-dbt-airflow-lib/src/dlh_airflow_common/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Common utilities for Airflow operations."""
-
-from dlh_airflow_common.utils.logging import get_logger
-
-__all__ = ["get_logger"]
